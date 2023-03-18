5340b5d53a7c4866df68360c3f3505fa9d94b499
timenotspace <timenotspace.eth@gmail.com>
2023-03-18 02:06:17 -0400

    ok let's try

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -4,8 +4,16 @@ import { OrbitControls, Grid } from '@react-three/drei';
 import { Bloom, EffectComposer } from '@react-three/postprocessing';
 import * as THREE from 'three';
 
+
+const CustomGridMaterial = new THREE.LineBasicMaterial({
+    color: new THREE.Color(0xffffff),
+    emissive: new THREE.Color(0xffffff),
+  });
+
 const StarField: React.FC = () => {
   const count = 5000;
+  const sphereRef = React.useRef<THREE.InstancedMesh>(null);
+
   const positions = useMemo(() => {
     const pos = new Float32Array(count * 3);
     const radius = 100;
@@ -22,53 +30,107 @@ const StarField: React.FC = () => {
 
     return pos;
   }, []);
+  
 
-  const geometry = new THREE.BufferGeometry();
-  geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+  React.useEffect(() => {
+    if (sphereRef.current) {
+      const tempObject = new THREE.Object3D();
+      for (let i = 0; i < count; i++) {
+        tempObject.position.set(positions[i * 3], positions[i * 3 + 1], positions[i * 3 + 2]);
+        tempObject.updateMatrix();
+        sphereRef.current.setMatrixAt(i, tempObject.matrix);
+      }
+      sphereRef.current.instanceMatrix.needsUpdate = true;
+    }
+  }, [positions]);
 
-  const material = new THREE.PointsMaterial({ size: 0.15, color: 'white' });
-
-  return <points geometry={geometry} material={material} />;
+  return (
+    <instancedMesh ref={sphereRef} args={[null, null, count]}>
+      <sphereGeometry args={[0.1, 32, 32]} />
+      <meshBasicMaterial color="white" />
+    </instancedMesh>
+  );
 };
 
 const MainScene: React.FC = () => {
-  return (
-    <>
-      <StarField />
-
-      {/* Middle orb */}
-      <mesh position={[0, 0, 0]}>
-        <sphereGeometry args={[1, 32, 32]} />
-        <meshBasicMaterial color={new THREE.Color(0xffffff)} />
-      </mesh>
-
-      {/* Grid plane */}
-      <Grid args={[10, 10]} color="#ffffff" />
-
-      {/* Add a light source */}
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[5, 5, 5]} />
-    </>
-  );
-};
-
-const Scene: React.FC = () => {
-  return (
-    <div className="canvas-container">
-      <Canvas>
-        <MainScene />
-        <EffectComposer>
-          <Bloom
-            intensity={1.5}
-            luminanceThreshold={0.9}
-            luminanceSmoothing={0.1}
-            height={300}
-          />
-        </EffectComposer>
-        <OrbitControls />
-      </Canvas>
-    </div>
-  );
-};
-
-export default Scene;
+    return (
+      <>
+        <StarField />
+  
+        {/* Middle orb */}
+        <mesh position={[0, 0, 0]}>
+          <sphereGeometry args={[1, 32, 32]} />
+          <meshBasicMaterial color={new THREE.Color(0xffffff)} />
+        </mesh>
+  
+        {/* Grid plane */}
+        <group position={[0, 0, 0]}>
+          {Array.from({ length: 21 }, (_, i) => (
+            <line key={i}>
+              <bufferGeometry>
+                <bufferAttribute
+                  attach="position"
+                  array={Float32Array.from([
+                    -10 + i,
+                    0,
+                    -10,
+                    -10 + i,
+                    0,
+                    10,
+                  ])}
+                  count={2}
+                  itemSize={3}
+                />
+              </bufferGeometry>
+              <primitive object={CustomGridMaterial} />
+            </line>
+          ))}
+          {Array.from({ length: 21 }, (_, i) => (
+            <line key={i}>
+              <bufferGeometry>
+                <bufferAttribute
+                  attach="position"
+                  array={Float32Array.from([
+                    -10,
+                    0,
+                    -10 + i,
+                    10,
+                    0,
+                    -10 + i,
+                  ])}
+                  count={2}
+                  itemSize={3}
+                />
+              </bufferGeometry>
+              <primitive object={CustomGridMaterial} />
+            </line>
+          ))}
+        </group>
+  
+        {/* Add a light source */}
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 5, 5]} />
+      </>
+    );
+  };
+  
+  const Scene: React.FC = () => {
+    return (
+      <div className="canvas-container">
+        <Canvas>
+          <MainScene />
+          <EffectComposer>
+            <Bloom
+              intensity={1.0}
+              luminanceThreshold={0.7}
+              luminanceSmoothing={0.1}
+              height={300}
+            />
+          </EffectComposer>
+          <OrbitControls />
+        </Canvas>
+      </div>
+    );
+  };
+  
+  export default Scene;
